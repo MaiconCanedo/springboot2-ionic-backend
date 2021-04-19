7e106228af23c576cd565a281c3c2eedbd40e794
MaiconCanedo <maicon.canedo1995@gmail.com>
2021-04-19 13:02:07 -0300

    refactor: Simplificando tipo de retorno dos métodos

--- src/main/java/com/nelioalves/cursomc/api/controller/ProdutoResource.java
+++ src/main/java/com/nelioalves/cursomc/api/controller/ProdutoResource.java
@@ -7,7 +7,6 @@ import com.nelioalves.cursomc.domain.service.ProdutoService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.data.domain.Sort.Direction.ASC;
@@ -23,22 +22,22 @@ public class ProdutoResource {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Produto> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(service.find(id));
+    public Produto findById(@PathVariable Integer id) {
+        return service.find(id);
     }
 
-    public ResponseEntity<Page<Produto>> findAll(
+    public Page<Produto> findAll(
             @PageableDefault(size = 24, sort = "nome", direction = ASC) Pageable pageable
     ) {
-        return ResponseEntity.ok(service.findPage(pageable));
+        return service.findPage(pageable);
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProdutoModel>> search(
+    public Page<ProdutoModel> findAll(
             @RequestParam(value = "nome", defaultValue = "") String nome,
             @RequestParam(value = "categorias", defaultValue = "") String categorias,
             @PageableDefault(size = 24, sort = "nome", direction = ASC) Pageable pageable
     ) {
-        return ResponseEntity.ok(service.search(URL.decodeParam(nome), URL.decodeIntList(categorias), pageable));
+        return service.search(URL.decodeParam(nome), URL.decodeIntList(categorias), pageable);
     }
 }
