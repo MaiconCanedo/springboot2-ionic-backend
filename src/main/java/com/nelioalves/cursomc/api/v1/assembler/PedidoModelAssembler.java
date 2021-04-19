c13cd87ee77fb66ee46f4179bfa1d9e9d866eab4
MaiconCanedo <maicon.canedo1995@gmail.com>
2021-04-19 20:00:39 -0300

    refactor: Criação do PedidoModel

--- src/main/java/com/nelioalves/cursomc/api/v1/assembler/PedidoModelAssembler.java
+++ src/main/java/com/nelioalves/cursomc/api/v1/assembler/PedidoModelAssembler.java
@@ -1,10 +1,12 @@
 package com.nelioalves.cursomc.api.v1.assembler;
 
+import com.nelioalves.cursomc.api.v1.model.PedidoModel;
+import com.nelioalves.cursomc.domain.entity.Pedido;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface PedidoModelAssembler {
 
-
+    PedidoModel toModel(Pedido pedido);
 
 }
---
+++ src/main/java/com/nelioalves/cursomc/api/v1/model/PedidoModel.java
@@ -0,0 +1,34 @@
+package com.nelioalves.cursomc.api.v1.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.nelioalves.cursomc.domain.entity.Cliente;
+import com.nelioalves.cursomc.domain.entity.Endereco;
+import com.nelioalves.cursomc.domain.entity.ItemPedido;
+import com.nelioalves.cursomc.domain.entity.Pagamento;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class PedidoModel {
+
+    private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private OffsetDateTime instante;
+
+    private Pagamento pagamento;
+
+    private Cliente cliente;
+
+    private Endereco enderecoDeEntrega;
+
+    private Set<ItemPedido> itens = new HashSet<>();
+
+}
