c24196aac0ff61d10016a302b43b946a9eb4d7f7
MaiconCanedo <maicon.canedo1995@gmail.com>
2021-05-05 18:39:40 -0300

    refactor: Criação da classe CidadeDTO

---
+++ src/main/java/com/nelioalves/cursomc/domain/entity/dto/CidadeDTO.java
@@ -0,0 +1,12 @@
+package com.nelioalves.cursomc.domain.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CidadeDTO {
+
+    private final Integer id;
+    private final String nome;
+}
